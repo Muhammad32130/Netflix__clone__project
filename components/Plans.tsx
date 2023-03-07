3dd21897f0c49fa4e1902eae01b35184b6021ea8
Muhammad32130 <askari.muhammad321@gmail.com>
2023-03-06 23:05:24 -0600

    Made a payments plan options with stripe

---
+++ components/Plans.tsx
@@ -0,0 +1,51 @@
+import { CheckIcon } from '@heroicons/react/solid'
+import { Product } from '@stripe/firestore-stripe-payments'
+import Head from 'next/head'
+import Link from 'next/link'
+import React from 'react'
+import useAuth from '../hooks/useAuth'
+import Table from './Table'
+
+interface Props{
+    products: Product[]
+}
+
+function Plans({products}:Props) {
+    const {logout} = useAuth()
+
+    return (
+        <div>
+            <Head>
+                <title>Netflix</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <header className='border-b border-white/10 bg-[#141414]'>
+                <Link href='/'>
+                    <img src="https://rb.gy/ulxxee" alt="Netflix" width={150} height={90} className="cursor-pointer object-contain" />
+                </Link>
+        <button className='text-lg font-medium hover:underline' onClick={logout}>Sign Out</button>
+            </header>
+            <main className='mx-atuo max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10'>
+                <h1 className='mb-3 text-3xl font-medium'>Choose the plan that's right for you</h1>
+                <ul>
+                    <li><CheckIcon className='h-7 w-7 text-[#E50914]'/> Watch all you want. Ad-free.</li>
+                    <li><CheckIcon className='h-7 w-7 text-[#E50914]'/> Recommendations just for you.</li>
+                    <li><CheckIcon className='h-7 w-7 text-[#E50914]'/> Change or cancel your plan anytime.</li>
+                </ul>
+                <div className='mt-4 flex flex-col space-y-4'>
+                    <div className='flex w-full items-center justify-end self-end md:w-3/5'>
+                       {products.map((product)=>(
+                        <div key={product.id} className="planBox">{product.name}</div>
+                       ))}
+                    </div>
+                    <Table products={products}></Table>
+
+                    <button>Subscribe</button>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Plans
---
+++ components/Table.tsx
@@ -0,0 +1,23 @@
+import { Product } from '@stripe/firestore-stripe-payments'
+import React from 'react'
+
+interface Props{
+    products: Product[]
+}
+
+function Table({ products }: Props) {
+  return (
+    <table>
+        <tbody>
+            <tr>
+                <td>Monthly price</td>
+                {products.map((product)=>(
+                    <td key={product.id}>${(product.prices[0].unit_amount/100).toFixed(2)}</td>
+                ))}
+            </tr>
+        </tbody>
+    </table>
+  )
+}
+
+export default Table
